412fcecb4bf4b748d64e425511c35dbcbd86f3d0
rahul-chekuri <rahulgandhi.chekuri@opsmx.io>
2021-12-09 19:49:31 +0530

    OP-10214: Moved code to appropriate packages

--- gate-ldap/src/main/java/opsmx/spinnaker/gate/security/ldap/RetryOnExceptionAuthManager.java
+++ gate-ldap/src/main/java/opsmx/spinnaker/gate/security/ldap/RetryOnExceptionAuthManager.java
@@ -1,4 +1,4 @@
-package com.netflix.spinnaker.gate.security.ldap;
+package opsmx.spinnaker.gate.security.ldap;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ldap.UncategorizedLdapException;
