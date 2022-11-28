c2f3aa8343f13c99d1c1e55526be89e515dcb9a5
Pranav-b-7 <pranav.bhaskaran@opsmx.io>
2022-11-28 17:57:52 +0530

    setting x-spinnaker-user header

--- gate-web/src/main/groovy/com/netflix/spinnaker/gate/interceptors/RequestIdInterceptor.java
+++ gate-web/src/main/groovy/com/netflix/spinnaker/gate/interceptors/RequestIdInterceptor.java
@@ -33,6 +33,7 @@ public class RequestIdInterceptor extends HandlerInterceptorAdapter {
       HttpServletRequest request, HttpServletResponse response, Object handler) {
     AuthenticatedRequest.getSpinnakerRequestId()
         .ifPresent(requestId -> response.setHeader(REQUEST_ID.getHeader(), requestId));
+    response.setHeader("x-spinnaker-user", request.getUserPrincipal().getName());
     return true;
   }
 }
