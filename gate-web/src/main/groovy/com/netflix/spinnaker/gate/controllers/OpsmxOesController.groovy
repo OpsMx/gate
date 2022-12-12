15a9bd8436980c1fbeab10518bdbae7779bf0dd4
sriharshakancharla <harsha.kancharla@opsmx.io>
2022-12-12 11:44:37 +0530

    OP-18661 - Added new query params for manifest API

--- gate-web/src/main/groovy/com/netflix/spinnaker/gate/controllers/OpsmxOesController.groovy
+++ gate-web/src/main/groovy/com/netflix/spinnaker/gate/controllers/OpsmxOesController.groovy
@@ -480,19 +480,27 @@ class OpsmxOesController {
 	  }
 	}
   }
-  @ApiOperation(value = "download the manifest file")
-  @GetMapping(value = "accountsConfig/agents/{agentName}/manifest", produces = "application/octet-stream")
-  @ResponseBody Object getDownloadAgentManifestFile(@PathVariable("agentName") String agentName){
 
-    Response response = opsmxOesService.agentManifestDownloadFile(agentName)
+  @ApiOperation(value = "download the manifest file")
+  @GetMapping(value = "/{type}/{source}/{source1}/manifest", produces = "application/octet-stream")
+  @ResponseBody
+  Object getDownloadAgentManifestFile(@PathVariable("type") String type,
+                                      @PathVariable("source") String source,
+                                      @PathVariable("source1") String source1,
+                                      @RequestParam(value = "description", required = false) String description,
+                                      @RequestParam(value = "isExists", required = false) Boolean isExists,
+                                      @RequestParam(value = "namespace", required = false) String namespace,
+                                      @RequestParam(value = "argoCdUrl", required = false) String argoCdUrl,
+                                      @RequestParam(value = "argoRolloutUrl", required = false) String argoRolloutUrl) {
+    Response response = opsmxOesService.agentManifestDownloadFile(type, source, source1, description, isExists, namespace, argoCdUrl, argoRolloutUrl)
     InputStream inputStream = response.getBody().in()
     try {
-      byte [] manifestFile = IOUtils.toByteArray(inputStream)
+      byte[] manifestFile = IOUtils.toByteArray(inputStream)
       HttpHeaders headers = new HttpHeaders()
       headers.add("Content-Disposition", response.getHeaders().stream().filter({ header -> header.getName().trim().equalsIgnoreCase("Content-Disposition") }).collect(Collectors.toList()).get(0).value)
       return ResponseEntity.ok().headers(headers).body(manifestFile)
-    } finally{
-      if (inputStream!=null){
+    } finally {
+      if (inputStream != null) {
         inputStream.close()
       }
     }
