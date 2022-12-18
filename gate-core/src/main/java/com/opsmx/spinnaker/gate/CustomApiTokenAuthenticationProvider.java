08181e2b8847742ead15c8c5baf91806a75bb23f
Ravikumar Hawaldar <ravikumar.hawaldar@opsmx.io>
2022-12-18 21:38:27 +0530

    OP-18516 : Added logic to authenticate the token via a request to Autopilot service. Also, added the X_SPINNAKER_USER header.

--- gate-core/src/main/java/com/opsmx/spinnaker/gate/CustomApiTokenAuthenticationProvider.java
+++ gate-core/src/main/java/com/opsmx/spinnaker/gate/CustomApiTokenAuthenticationProvider.java
@@ -1,22 +1,9 @@
-/*
- * Copyright 2022 Netflix, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.opsmx.spinnaker.gate;
 
+import com.netflix.spinnaker.gate.model.TokenModel;
+import com.netflix.spinnaker.gate.services.TokenValidationService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
@@ -29,18 +16,28 @@ import org.springframework.util.ObjectUtils;
 @Component
 public class CustomApiTokenAuthenticationProvider implements AuthenticationProvider {
 
+  @Autowired private TokenValidationService tokenValidationService;
+
   @Override
   public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-    //    String apiToken = (String) authentication.getPrincipal();
     String apiToken = (String) authentication.getCredentials();
-    log.info("*****************#### Api token is {} ", apiToken);
+
     if (ObjectUtils.isEmpty(apiToken)) {
-      throw new InsufficientAuthenticationException("No API key in request");
+      throw new InsufficientAuthenticationException("No API token is request");
     } else {
-      if ("ValidApiKey".equals(apiToken)) {
-        return new CustomApiTokenAuthentication(apiToken, true);
+      String[] values = apiToken.split(";");
+      TokenModel tokenModel =
+          tokenValidationService
+              .validateTokenByCanaryRunId(Integer.valueOf(values[2]), values[3])
+              .getBody();
+      if (tokenModel != null && tokenModel.isValidToken()) {
+        String username = tokenModel.getUserName();
+        CustomApiTokenAuthentication customApiTokenAuthentication =
+            new CustomApiTokenAuthentication(apiToken, true);
+        customApiTokenAuthentication.setUsername(username);
+        return customApiTokenAuthentication;
       }
-      throw new BadCredentialsException("API Key is invalid");
+      throw new BadCredentialsException("API token is invalid");
     }
   }
 
--- gate-core/src/main/java/com/opsmx/spinnaker/gate/CustomTokenAuthenticationFilter.java
+++ gate-core/src/main/java/com/opsmx/spinnaker/gate/CustomTokenAuthenticationFilter.java
@@ -1,21 +1,7 @@
-/*
- * Copyright 2022 Netflix, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.opsmx.spinnaker.gate;
 
+import com.netflix.spinnaker.kork.common.Header;
+import com.netflix.spinnaker.security.AuthenticatedRequest;
 import java.io.IOException;
 import java.util.Optional;
 import javax.servlet.FilterChain;
@@ -30,72 +16,22 @@ import org.springframework.security.web.authentication.AbstractAuthenticationPro
 
 @Slf4j
 public class CustomTokenAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
-  //  private final static UrlPathHelper urlPathHelper = new UrlPathHelper();
 
   public CustomTokenAuthenticationFilter(AuthenticationManager authenticationManager) {
     super("/**");
     this.setAuthenticationManager(authenticationManager);
   }
 
-  /* @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException,
-      ServletException {
-      HttpServletRequest request = (HttpServletRequest) req;
-      HttpServletResponse response = (HttpServletResponse) res;
-
-      if (!this.requiresAuthentication(request, response)) {
-        chain.doFilter(request, response);
-      } else {
-        log.info("*************** Request is to process authentication");
-      }
-        boolean success = true;
-
-        Authentication authResult = null;
-        try {
-          Optional<String> apiTokenOptional = Optional.ofNullable(request.getHeader("x-opsmx-token"));
-          if(apiTokenOptional.isPresent()){
-            log.info("*************** calling authentication function");
-            authResult = this.attemptAuthentication(request, response);
-          }else{
-            log.info("*************** continuing the filter");
-  //          AbstractAuthenticationToken auth = new AbstractAuthenticationToken();
-  //          CustomApiTokenAuthentication token = new CustomApiTokenAuthentication(null);
-  //          getAuthenticationManager().authenticate(token);
-            SecurityContextHolder.getContext().setAuthentication(null);
-            chain.doFilter(request, response);
-  //          return;
-          }
-        } catch (InternalAuthenticationServiceException var8) {
-          this.logger.error("An internal error occurred while trying to authenticate the user.", var8);
-          success = false;
-        } catch (AuthenticationException var9) {
-          success = false;
-        }
-
-
-        if (success && null != authResult) {
-          this.successfulAuthentication(request, response, chain, authResult);
-        }
-
-        // Please ensure that chain.doFilter(request, response) is invoked upon successful authentication. You want
-        // processing of the request to advance to the next filter, because very last one filter
-        // FilterSecurityInterceptor#doFilter is responsible to actually invoke method in your controller that is
-        // handling requested API resource.
-  //      chain.doFilter(request, response);
-      }*/
-
   @Override
   public Authentication attemptAuthentication(
       HttpServletRequest request, HttpServletResponse response) {
 
     Optional<String> apiTokenOptional = Optional.ofNullable(request.getHeader("x-opsmx-token"));
-    log.info("******************# Request Header is {} ", apiTokenOptional);
 
     CustomApiTokenAuthentication token =
         apiTokenOptional
             .map(CustomApiTokenAuthentication::new)
             .orElse(new CustomApiTokenAuthentication());
-    log.info("****************** Token Object {} ", token);
     return getAuthenticationManager().authenticate(token);
   }
 
@@ -108,24 +44,7 @@ public class CustomTokenAuthenticationFilter extends AbstractAuthenticationProce
       throws IOException, ServletException {
 
     SecurityContextHolder.getContext().setAuthentication(authResult);
-    log.info(
-        "********** authentication {}",
-        SecurityContextHolder.getContext().getAuthentication().getName());
-    log.info(
-        "********** details {}", SecurityContextHolder.getContext().getAuthentication().toString());
+    AuthenticatedRequest.set(Header.USER.getHeader(), (String) authResult.getPrincipal());
     chain.doFilter(request, response);
   }
-
-  //  @Override
-  //  protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse
-  // response,
-  //                                            AuthenticationException failed) throws IOException,
-  // ServletException {
-  //    log.info("************* failed authentication while attempting to access "
-  //      + urlPathHelper.getPathWithinApplication((HttpServletRequest) request));
-  //
-  //    //Add more descriptive message
-  //    response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
-  //      "Authentication Failed");
-  //  }
 }
