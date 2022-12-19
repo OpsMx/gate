52ff874a4b5760561a55111239a6cb14e44d33e9
Ravikumar Hawaldar <ravikumar.hawaldar@opsmx.io>
2022-12-19 18:31:47 +0530

    OP-18516 : Fix for UI continuous loading issue. Also, added report page APIs which needs token auth.

--- gate-core/src/main/java/com/opsmx/spinnaker/gate/CustomTokenSecurityConfig.java
+++ gate-core/src/main/java/com/opsmx/spinnaker/gate/CustomTokenSecurityConfig.java
@@ -42,7 +42,67 @@ public class CustomTokenSecurityConfig extends WebSecurityConfigurerAdapter {
         .antMatchers("/health")
         .antMatchers("/prometheus")
         .antMatchers("/info")
-        .antMatchers("/metrics");
+        .antMatchers("/metrics")
+        .antMatchers("/auth/login")
+        .antMatchers("/auth/logout")
+        .antMatchers("/login")
+        .antMatchers("/session")
+        .antMatchers("/redirectauto")
+        .antMatchers(HttpMethod.POST, "/autopilot/registerCanary")
+        .antMatchers(HttpMethod.GET, "/autopilot/api/v2/autopilot/canaries/{id}")
+        .antMatchers(HttpMethod.GET, "/autopilot/api/v1/autopilot/canaries/{id}")
+        .antMatchers(HttpMethod.POST, "/autopilot/api/v1/registerCanary")
+        .antMatchers(HttpMethod.POST, "/autopilot/api/v2/registerCanary")
+        .antMatchers(HttpMethod.POST, "/autopilot/api/v3/registerCanary")
+        .antMatchers(HttpMethod.POST, "/autopilot/api/v5/registerCanary")
+        .antMatchers(HttpMethod.GET, "/autopilot/canaries/{id}")
+        .antMatchers(HttpMethod.GET, "/autopilot/v5/canaries/{id}")
+        .antMatchers(HttpMethod.GET, "/autopilot/api/v5/external/template")
+        .antMatchers(HttpMethod.POST, "/autopilot/api/v5/external/template")
+        .antMatchers(HttpMethod.POST, "/visibilityservice/v1/approvalGates/{id}/trigger")
+        .antMatchers(HttpMethod.POST, "/visibilityservice/v2/approvalGates/{id}/trigger")
+        .antMatchers(HttpMethod.POST, "/visibilityservice/v4/approvalGates/{id}/trigger")
+        .antMatchers(HttpMethod.POST, "/visibilityservice/v5/approvalGates/{id}/trigger")
+        .antMatchers(HttpMethod.GET, "/visibilityservice/v2/approvalGateInstances/{id}/status")
+        .antMatchers(HttpMethod.GET, "/visibilityservice/v1/approvalGateInstances/{id}/status")
+        .antMatchers(
+            HttpMethod.PUT, "/visibilityservice/v1/approvalGateInstances/{id}/spinnakerReview")
+        .antMatchers(HttpMethod.POST, "/oes/echo")
+        .antMatchers(HttpMethod.POST, "/oes/echo/")
+        .antMatchers(HttpMethod.POST, "/auditservice/v1/echo/events/data")
+        .antMatchers(HttpMethod.POST, "/auditservice/v1/echo/events/data/")
+        .antMatchers(HttpMethod.POST, "/v1/data/**")
+        .antMatchers(HttpMethod.POST, "/v1/staticPolicy/eval")
+        .antMatchers(HttpMethod.POST, "/v1/staticPolicy/eval/")
+        .antMatchers(HttpMethod.GET, "/autopilot/mgmt/**")
+        .antMatchers(HttpMethod.POST, "/datasource/cache/save")
+        .antMatchers(HttpMethod.DELETE, "/datasource/cache/evict")
+        .antMatchers("/plugins/deck/**")
+        .antMatchers(HttpMethod.POST, "/webhooks/**")
+        .antMatchers(HttpMethod.POST, "/notifications/callbacks/**")
+        .antMatchers(HttpMethod.POST, "/managed/notifications/callbacks/**")
+        .antMatchers(HttpMethod.GET, "/oes/accountsConfig/v2/agents/apple/automation")
+        .antMatchers(HttpMethod.POST, "/oes/accountsConfig/v1/agents/apple/automation")
+        .antMatchers(
+            HttpMethod.GET, "/oes/accountsConfig/v1/agents/{agentName}/manifest/apple/automation")
+        .antMatchers(
+            HttpMethod.GET,
+            "/oes/accountsConfig/v2/spinnaker/cloudProviderAccount/apple/automation")
+        .antMatchers(
+            HttpMethod.GET,
+            "/oes/accountsConfig/v2/spinnaker/cloudProviderAccount/{agentName}/{accountName}/apple/automation")
+        .antMatchers(
+            HttpMethod.POST,
+            "/oes/accountsConfig/v2/spinnaker/cloudProviderAccount/apple/automation")
+        .antMatchers(HttpMethod.GET, "/oes/accountsConfig/v3/spinnaker/apple/automation")
+        .antMatchers(HttpMethod.GET, "/dashboardservice/v4/getAllDatasources/apple/automation")
+        .antMatchers(
+            HttpMethod.GET,
+            "/dashboardservice/v5/agents/{agentName}/accounts/{accountName}/accountType/{accountType}/apple/automation")
+        .antMatchers(HttpMethod.POST, "/dashboardservice/v4/datasource/apple/automation")
+        .antMatchers(
+            HttpMethod.GET,
+            "/platformservice/v6/applications/{applicationname}/pipeline/{pipelineName}/reference/{ref}/gates/{gatesName}");
   }
 
   @Override
@@ -56,8 +116,29 @@ public class CustomTokenSecurityConfig extends WebSecurityConfigurerAdapter {
         .requestCache()
         .disable()
         .antMatcher("/platformservice/v7/argo/cdIntegrations")
-        .antMatcher("/platformservice/v1/applications")
+        // Start of report page APIs, Need to remove them once token based authentication is in
+        // place.
+        .antMatcher("/autopilot/api/v2/permissions/getApplications")
+        .antMatcher("/autopilot/api/v1/applications/{source1}/tags")
+        .antMatcher("/autopilot/api/v2/applications/getApplicationHealth")
+        .antMatcher("/autopilot/api/v1/defaultLogTemplate")
+        .antMatcher("/autopilot/cas/getCanaryOutputNew")
+        .antMatcher("/autopilot/api/v1/correlation/log/{source1}/{source2}")
+        .antMatcher("/autopilot/api/v1/correlation/metric")
+        .antMatcher("/autopilot/logs/feedbackHistory")
         .antMatcher("/autopilot/api/v5/global/template")
+        .antMatcher("/autopilot/api/v4/monitoringproviders/log")
+        .antMatcher("/autopilot/api/v1/scoring-algorithms")
+        .antMatcher("/platformservice/v6/users/{source1}/datasources")
+        .antMatcher("/autopilot/api/v4/monitoringproviders/custom")
+        .antMatcher("/autopilot/api/v4/monitoringproviders/apm")
+        .antMatcher("/dashboardservice/v2/users/{source1}/applications/latest-canary")
+        .antMatcher("/autopilot/api/v1/getClusterTags")
+        .antMatcher("/autopilot/logs/getDataSourceResponseKeys")
+        .antMatcher("/autopilot/logs/updateFeedbackLogTemplate")
+        .antMatcher("/platformservice/v1/users/{username}/verifications/applications")
+        .antMatcher("/autopilot/api/v1/correlation/metric/{riskAnalysisId}/{serviceId}")
+        // End of report page APIs
         .authorizeRequests()
         .anyRequest()
         .authenticated()
