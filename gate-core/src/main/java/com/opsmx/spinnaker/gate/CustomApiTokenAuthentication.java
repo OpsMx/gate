d3f202457613cbf097783d6842484360182f4b13
Ravikumar Hawaldar <94454302+rsh09@users.noreply.github.com>
2022-12-21 10:32:39 +0530

    OP-18516 : Token based authentication mechanism supporting changes (#299)

---
+++ gate-core/src/main/java/com/opsmx/spinnaker/gate/CustomApiTokenAuthentication.java
@@ -0,0 +1,79 @@
+package com.opsmx.spinnaker.gate;
+
+import java.util.Objects;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.Transient;
+import org.springframework.security.core.authority.AuthorityUtils;
+
+/*
+   A marker for Authentications that should never be stored across requests, for example a bearer token authentication
+*/
+@Transient
+public class CustomApiTokenAuthentication extends AbstractAuthenticationToken {
+
+  private String apiToken;
+  private String username;
+  private Integer canaryId;
+
+  /*
+   GrantedAuthority as being a "permission" or a "right". Those "permissions" are (normally) expressed as
+   strings (with the getAuthority() method). Those strings let you identify the permissions and let your
+   voters decide if they grant access to something.
+  */
+  public CustomApiTokenAuthentication(String apiToken, boolean authenticated) {
+    super(AuthorityUtils.NO_AUTHORITIES);
+    this.apiToken = apiToken;
+    setAuthenticated(authenticated);
+  }
+
+  public CustomApiTokenAuthentication(String apiToken) {
+    super(AuthorityUtils.NO_AUTHORITIES);
+    this.apiToken = apiToken;
+    setAuthenticated(false);
+  }
+
+  public CustomApiTokenAuthentication() {
+    super(AuthorityUtils.NO_AUTHORITIES);
+    setAuthenticated(false);
+  }
+
+  @Override
+  public Object getCredentials() {
+    return apiToken;
+  }
+
+  /*
+   The identity of the principal being authenticated. In the case of an authentication
+   request with username and password, this would be the username.
+  */
+  @Override
+  public Object getPrincipal() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public Integer getCanaryId() {
+    return canaryId;
+  }
+
+  public void setCanaryId(Integer canaryId) {
+    this.canaryId = canaryId;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    if (!super.equals(o)) return false;
+    CustomApiTokenAuthentication that = (CustomApiTokenAuthentication) o;
+    return Objects.equals(username, that.username);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), username);
+  }
+}
---
+++ gate-core/src/main/java/com/opsmx/spinnaker/gate/CustomApiTokenAuthenticationProvider.java
@@ -0,0 +1,57 @@
+package com.opsmx.spinnaker.gate;
+
+import com.netflix.spinnaker.gate.model.TokenModel;
+import com.netflix.spinnaker.gate.services.TokenValidationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+
+@Slf4j
+@Component
+public class CustomApiTokenAuthenticationProvider implements AuthenticationProvider {
+
+  @Autowired private TokenValidationService tokenValidationService;
+
+  @Override
+  public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+    String apiToken = (String) authentication.getCredentials();
+
+    if (ObjectUtils.isEmpty(apiToken)) {
+      throw new InsufficientAuthenticationException("No API token present in the request header");
+    } else {
+      String[] values = apiToken.split(";");
+      Integer canaryId = Integer.valueOf(values[2]);
+
+      CustomApiTokenAuthentication token = (CustomApiTokenAuthentication) authentication;
+      Integer canaryIdFromRequestParam = token.getCanaryId();
+
+      if (canaryIdFromRequestParam != null) {
+        canaryId = canaryIdFromRequestParam;
+      }
+
+      TokenModel tokenModel =
+          tokenValidationService.validateTokenByCanaryRunId(canaryId, values[3]).getBody();
+
+      if (tokenModel != null && tokenModel.isValidToken()) {
+        String username = tokenModel.getUserName();
+        CustomApiTokenAuthentication customApiTokenAuthentication =
+            new CustomApiTokenAuthentication(apiToken, true);
+        customApiTokenAuthentication.setUsername(username);
+
+        return customApiTokenAuthentication;
+      }
+      throw new BadCredentialsException("API token is invalid");
+    }
+  }
+
+  @Override
+  public boolean supports(Class<?> authentication) {
+    return CustomApiTokenAuthentication.class.isAssignableFrom(authentication);
+  }
+}
