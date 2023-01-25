fde3d66febe0886486de2f84230d996e953bff45
srikanthopsmx <srikanth@opsmx.io>
2023-01-25 13:56:55 +0530

    OP-18982-Master  jwt token validation changes

--- gate-core/src/main/java/com/opsmx/spinnaker/gate/CustomApiTokenAuthenticationProvider.java
+++ gate-core/src/main/java/com/opsmx/spinnaker/gate/CustomApiTokenAuthenticationProvider.java
@@ -1,7 +1,15 @@
 package com.opsmx.spinnaker.gate;
 
+import com.google.gson.Gson;
 import com.netflix.spinnaker.gate.model.TokenModel;
 import com.netflix.spinnaker.gate.services.TokenValidationService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.math.BigDecimal;
+import java.util.Base64;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
@@ -19,6 +27,7 @@ import org.springframework.util.ObjectUtils;
 public class CustomApiTokenAuthenticationProvider implements AuthenticationProvider {
 
   @Autowired private TokenValidationService tokenValidationService;
+  private Gson gson = new Gson();
 
   @Override
   public Authentication authenticate(Authentication authentication) throws AuthenticationException {
@@ -26,6 +35,45 @@ public class CustomApiTokenAuthenticationProvider implements AuthenticationProvi
 
     if (ObjectUtils.isEmpty(apiToken)) {
       throw new InsufficientAuthenticationException("No API token present in the request header");
+    } else if (apiToken.startsWith("jwt")) {
+      Map<String, Object> header = new LinkedHashMap<>();
+      header.put("alg", SignatureAlgorithm.HS256.name());
+      header.put("typ", "JWT");
+      String[] array = apiToken.split(";");
+      String[] values = array[1].split("[.]");
+      String token = array[1].trim();
+      byte[] decodedBytes = Base64.getDecoder().decode(values[1]);
+      String decodedString = new String(decodedBytes);
+      Map<String, Object> map = gson.fromJson(decodedString, Map.class);
+      String sub = map.get("sub").toString().split(":")[0];
+      String iatValue = map.get("iat").toString();
+      BigDecimal bd = new BigDecimal(iatValue);
+      long iat = bd.longValue();
+      Date date = new Date(iat * 1000);
+      String key =
+          String.format(
+              "%s/%s/GOl@ngCOntrollerH@ndshake-@OPSMX-JavABACKeND|udfPLQZS",
+              String.valueOf(iat), sub);
+      String encodekey = Base64.getEncoder().encodeToString(key.getBytes());
+
+      String jwtToken =
+          Jwts.builder()
+              .claim("iss", "controller")
+              .setSubject(map.get("sub").toString())
+              .setIssuedAt(date)
+              .signWith(SignatureAlgorithm.HS256, encodekey)
+              .setHeader(header)
+              .compact();
+
+      if (!jwtToken.equals(token)) {
+        throw new BadCredentialsException("API token is invalid");
+      } else {
+        CustomApiTokenAuthentication customApiTokenAuthentication =
+            new CustomApiTokenAuthentication(apiToken, true);
+        customApiTokenAuthentication.setUsername(sub);
+        return customApiTokenAuthentication;
+      }
+
     } else {
       String[] values = apiToken.split(";");
 
