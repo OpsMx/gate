9f6fac074cb885dc0f3c795e0860863051d7c14b
sriharshakancharla <harsha.kancharla@opsmx.io>
2023-08-17 15:26:12 +0530

    OP-20902 : removed logs

--- gate-web/src/main/groovy/com/netflix/spinnaker/gate/controllers/OpsmxSsdController.groovy
+++ gate-web/src/main/groovy/com/netflix/spinnaker/gate/controllers/OpsmxSsdController.groovy
@@ -251,8 +251,6 @@ class OpsmxSsdController {
       try {
         byte[] jsonFile = IOUtils.toByteArray(inputStream)
         headers.setContentType(MediaType.parseMediaType("application/json"));
-        log.info(response.getHeaders().stream().filter({ header -> header.getName().trim().equalsIgnoreCase("Content-Disposition") }).collect(Collectors.toList()).get(0).value)
-        log.info(response.getHeaders().stream().filter({ header -> header.getName().trim().equalsIgnoreCase("Content-Disposition") }).collect(Collectors.toList()))
         headers.add("Content-Disposition", "attachment;filename=reports.json")
        //headers.add("Content-Disposition", response.getHeaders().stream().filter({ header -> header.getName().trim().equalsIgnoreCase("Content-Disposition") }).collect(Collectors.toList()).get(0).value)
         log.info("after " +response.getBody().toString())
