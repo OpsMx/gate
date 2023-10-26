57e00b6ef8721f8836cbb797ee40258b896614fe
sriharshakancharla <harsha.kancharla@opsmx.io>
2023-10-26 19:18:13 +0530

    OP-21304 : Update cluster APIs

--- gate-web/src/main/groovy/com/netflix/spinnaker/gate/controllers/OpsmxSsdController.groovy
+++ gate-web/src/main/groovy/com/netflix/spinnaker/gate/controllers/OpsmxSsdController.groovy
@@ -356,7 +356,7 @@ class OpsmxSsdController {
       def error = obj.body().string();
       throw new OesRequestException(error)
     } else {
-      return obj.body()?.string() ?: "Unknown reason: " + obj.code() as Object
+      return obj ?: "Unknown reason: " + obj.code() as Object
     }
   }
 
@@ -385,7 +385,7 @@ class OpsmxSsdController {
       def error = obj.body().string();
       throw new OesRequestException(error)
     } else {
-      return obj.body()?.string() ?: "Unknown reason: " + obj.code() as Object
+      return obj ?: "Unknown reason: " + obj.code() as Object
     }
   }
 
