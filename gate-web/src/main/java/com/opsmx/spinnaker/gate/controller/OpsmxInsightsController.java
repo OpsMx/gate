812506243144c553e78587fba3783bf919a46255
rahul-chekuri <rahulgandhi.chekuri@opsmx.io>
2023-12-13 14:12:03 +0530

    OP-20657: Disable controller.

--- gate-web/src/main/java/com/opsmx/spinnaker/gate/controller/OpsmxInsightsController.java
+++ gate-web/src/main/java/com/opsmx/spinnaker/gate/controller/OpsmxInsightsController.java
@@ -26,12 +26,10 @@ import okhttp3.*;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/insights")
-@RestController
+// @RequestMapping("/insights")
+// @RestController
 @Slf4j
 public class OpsmxInsightsController {
 
