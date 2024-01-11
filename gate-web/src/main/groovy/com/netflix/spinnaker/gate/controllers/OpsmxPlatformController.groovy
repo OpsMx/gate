2743c465f39c0c350d261f0d9de0545017aa04ba
vivek-opsmx <vivek.k@opsmx.io>
2024-01-11 14:21:11 +0530

    Added required = false

--- gate-web/src/main/groovy/com/netflix/spinnaker/gate/controllers/OpsmxPlatformController.groovy
+++ gate-web/src/main/groovy/com/netflix/spinnaker/gate/controllers/OpsmxPlatformController.groovy
@@ -313,8 +313,8 @@ class OpsmxPlatformController {
                                @RequestParam(value = "argoCdUrl", required = false) String argoCdUrl,
                                @RequestParam(value = "rolloutsEnabled", required = false) Boolean rolloutsEnabled,
                                @RequestParam(value = "isdUrl", required = false) String isdUrl,
-                               @RequestParam(value = "argoName") String argoName,
-                               @RequestParam(value = "agentName") String agentName,
+                               @RequestParam(value = "argoName",required = false) String argoName,
+                               @RequestParam(value = "agentName",required = false) String agentName,
                                @RequestBody(required = false) Object data) {
 
     return opsmxPlatformService.postPlatformResponse4(version, type, source, source1, isExists, description, namespace, argoCdUrl, rolloutsEnabled, isdUrl, argoName, agentName, data)
