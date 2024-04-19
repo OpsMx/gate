4279ec3a05d3b49fc4ee409e89fc498415c49175
vivek-opsmx <vivek.k@opsmx.io>
2024-04-19 16:32:45 +0530

    OP-21723 : Removed ConditionalOnExpression given false

--- gate-web/src/main/groovy/com/netflix/spinnaker/gate/config/GateConfig.groovy
+++ gate-web/src/main/groovy/com/netflix/spinnaker/gate/config/GateConfig.groovy
@@ -506,7 +506,6 @@ class GateConfig extends RedisHttpSessionConfiguration {
   }
 
   @Bean
-  @ConditionalOnExpression('${services.fiat.enabled:false}')
   FiatPermissionEvaluator fiatPermissionEvaluator(FiatStatus fiatStatus,
                                                   Registry registry,
                                                   FiatService fiatService,
@@ -514,7 +513,6 @@ class GateConfig extends RedisHttpSessionConfiguration {
     return new FiatPermissionEvaluator(registry, fiatService, fiatClientConfigurationProperties, fiatStatus)
   }
   @Bean
-  @ConditionalOnExpression('${services.fiat.enabled:false}')
   static MethodSecurityExpressionHandler expressionHandler(
     Registry registry,
     FiatService fiatService,
