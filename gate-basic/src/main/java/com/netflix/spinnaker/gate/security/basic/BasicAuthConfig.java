941174a921d53f8ddaefca3b363df540f75c9f17
vivek-opsmx <vivek.k@opsmx.io>
2024-05-06 09:57:19 +0530

    OP-21723 : Added Logs  In /login url

--- gate-basic/src/main/java/com/netflix/spinnaker/gate/security/basic/BasicAuthConfig.java
+++ gate-basic/src/main/java/com/netflix/spinnaker/gate/security/basic/BasicAuthConfig.java
@@ -98,6 +98,7 @@ public class BasicAuthConfig {
         .httpBasic()
         .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"));
     authConfig.configure(http);
+    log.info(" added logs in  /login URL Method : {}", http.build());
     return http.build();
   }
 
