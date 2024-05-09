c2f07176e501cfe4e4decd92e0030cd0c6fdd75c
vivek-opsmx <vivek.k@opsmx.io>
2024-05-09 20:34:31 +0530

    OP-21723 : Added SAML2 code

--- gate-saml/src/main/java/com/opsmx/spinnaker/gate/security/saml/Saml2UserAttributeMapping.java
+++ gate-saml/src/main/java/com/opsmx/spinnaker/gate/security/saml/Saml2UserAttributeMapping.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Netflix, Inc.
+ * Copyright 2024 OpsMx, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
