1a84624c832107e43f345274ee61eca8cf0c84cc
rahul-chekuri <rahulgandhi.chekuri@opsmx.io>
2024-07-03 17:02:47 +0530

    OP-22352: removed hasPermission for pipeline.

--- gate-web/src/main/groovy/com/netflix/spinnaker/gate/controllers/PipelineController.groovy
+++ gate-web/src/main/groovy/com/netflix/spinnaker/gate/controllers/PipelineController.groovy
@@ -262,7 +262,7 @@ class PipelineController {
   }
 
   @Operation(summary = "Trigger a pipeline execution")
-  @PreAuthorize("hasPermission(#application, 'APPLICATION', 'EXECUTE') && hasPermission(@urlUtils.decode(#pipelineNameOrId), 'PIPELINE', 'EXECUTE')")
+  @PreAuthorize("hasPermission(#application, 'APPLICATION', 'EXECUTE')")
   @PostMapping("/v2/{application}/{pipelineNameOrId:.+}")
   HttpEntity invokePipelineConfigViaEcho(@PathVariable("application") String application,
                                          @PathVariable("pipelineNameOrId") String pipelineNameOrId,
