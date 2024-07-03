f6ca0f0e47ff61fceb234005b72f0fa556278d42
rahul-chekuri <rahulgandhi.chekuri@opsmx.io>
2024-07-03 15:59:16 +0530

    OP-22352: added logs.

--- gate-web/src/main/groovy/com/netflix/spinnaker/gate/utils/UrlUtils.groovy
+++ gate-web/src/main/groovy/com/netflix/spinnaker/gate/utils/UrlUtils.groovy
@@ -1,12 +1,17 @@
 package com.netflix.spinnaker.gate.utils
 
+import groovy.util.logging.Slf4j
 import org.springframework.stereotype.Component
 
 import java.nio.charset.StandardCharsets
 
 @Component
+@Slf4j
 class UrlUtils {
   static String decode(String encodedValue) {
-    return URLDecoder.decode(encodedValue, StandardCharsets.UTF_8.toString())
+    log.info("encoded: " + encodedValue)
+    final decoded = URLDecoder.decode(encodedValue, StandardCharsets.UTF_8.toString())
+    log.info("decoded: " + decoded)
+    return decoded
   }
 }
